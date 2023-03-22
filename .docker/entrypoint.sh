517f422d4bc5609b15f982a9a97f52754fba19c2
Frederico Cruz <frederico.cruz@castgroup.com.br>
2023-03-22 11:48:15 -0300

    Docker: Ajustes gerais.

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -7,22 +7,25 @@ echo " \033[01;31m # Copiando arquivo default para nginx...                     
 cp default /etc/nginx/sites-available/default
 
 
+
+echo " \033[01;31m # Setando permissões de arquivos...                                          "
 chmod -R 777 /var/www/html
+
 
 
 if [ -d "/var/www/html/vendor" ]; then
     echo "Diretório vendor já existe!"
 else 
     if [ ! -f "/var/www/html/composer.phar" ]; then
-        echo " \033[01;31m # Baixando composer.phar...                                              "
+        echo " \033[01;31m # Baixando composer.phar...                                          "
         wget https://getcomposer.org/download/2.0.0/composer.phar /var/www/html
     fi
     
-    echo " \033[01;31m # Setando permissões de arquivos... "
+    echo " \033[01;31m # Setando permissões de arquivos...                                      "
     chmod +x /var/www/html/composer.phar
 
     echo " \033[01;31m # Executando composer.phar update...                                     "
-    /var/www/html/composer.phar update --ignore-platform-reqs 
+    /var/www/html/composer.phar install --ignore-platform-reqs 
     php artisan key:generate
 fi
 
@@ -31,6 +34,7 @@ fi
 echo " \033[01;31m # Setando permissões de arquivos... "
 chmod -R 775 /var/www/html
 chmod -R 777 /var/www/html/storage/
+
 
 
 echo " \033[01;33m###################################################################\033[01;33m"
